bf4469cb50c1aa05faaed36bc712593ff426bba7
lindyz <mkz.attorneyatlaw@hotmail.com>
2017-03-02 08:36:23 -0500

    Added two more classes - added seed data

--- Inventory2/Inventory2/Models/Category.cs
+++ Inventory2/Inventory2/Models/Category.cs
@@ -5,7 +5,7 @@ namespace Inventory2.Models
 {
     public class Category
     {
-        [ScaffoldColumn(false)]
+        [ScaffoldColumn(false)] //data annotation. describes how to validate user input for this member, to specify formatting for it, and to specify how it is modeled when the database is created//
         public int CategoryID { get; set; }
 
         [Required, StringLength(100), Display(Name = "Name")]
---
+++ Inventory2/Inventory2/Models/ProductContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+namespace Inventory2.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext() : base("WingtipToys")
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
