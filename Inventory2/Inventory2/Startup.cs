4176e16eff861ad0e262ce08ab021f16f8fd4c40
lindyz <mkz.attorneyatlaw@hotmail.com>
2017-03-02 07:21:07 -0500

    First Commit

---
+++ Inventory2/Inventory2/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Inventory2.Startup))]
+namespace Inventory2
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
