69b976c4d79c4602afd9cdf44162dee097184dc1
lindyz <mkz.attorneyatlaw@hotmail.com>
2017-03-07 07:27:14 -0500

    created admin page to login and make changes to the products

--- Inventory2/Inventory2/Site.Master.cs
+++ Inventory2/Inventory2/Site.Master.cs
@@ -72,7 +72,10 @@ namespace Inventory2
 
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (HttpContext.Current.User.IsInRole("canEdit"))
+            {
+                adminLink.Visible = true;
+            }
         }
 
         protected void Page_PreRender(object sender, EventArgs e)
